38b6b21737c42a936f33003e536f4b4e38f528a0
Robby <mrburninator@yahoo.com>
2015-02-07 20:12:06 -0800

    MPI Sample Code
    Added sample MPI code for sending data, has 3 examples:
    -sending a single double
    -sending a small array of doubles
    -sending the first level of u

--- mg.c
+++ mg.c
@@ -219,6 +219,33 @@ int main(int argc, const char **argv)
 	    if (timeron) timer_start(T_resid2);
 	    resid(u[0],v,r[0],n1,n2,n3,a);
 	    if (timeron) timer_stop(T_resid2);
+	/*
+		// REAL number;
+		// REAL * tempa = (REAL*) malloc(sizeof(REAL)*10);
+		REAL * tempdzyx = (REAL*) malloc(sizeof(REAL)*n1*n2*n3);
+		//assign values to temp
+		int i3, i2, i1;
+		double ***z = u[0];
+		//maps 3-D to 1-D array for transmission
+		for(i3=0;i3<n3;i3++){
+		        for(i2=0;i2<n2;i2++){
+		            for(i1=0;i1<n1;i1++){
+					tempdzyx[i3*n1*n2 + i2*n1 + i1] = z[i3][i2][i1];
+				}
+			}
+		}
+		//making the first element 0.1 because it will probably be 0.0- which makes it hard to tell if it actually transmitted correctly
+		tempdzyx[0] = 0.1;
+		REAL * tempa = (REAL*) malloc(sizeof(REAL)*10);
+		for(i1=0;i1<10;i1++){
+			tempa[i1] = tempdzyx[i1] + 1.0;
+		}
+
+		number = -1.5;
+		// MPI_Send(&number, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+		// MPI_Send(tempa, 10, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+		MPI_Send(tempdzyx, n1*n2*n3, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+	*/
 
 	    //nit == number of iterations
 	    for(it=1;it<=nit;it++) {
@@ -294,7 +321,18 @@ int main(int argc, const char **argv)
 	    freeGrids(r);
 	    free(v);
 
-	/*}
+	/*
+	} else {
+		// PPF_Print( MPI_COMM_WORLD, "Its the other processor!" );
+		// REAL number;
+		// REAL * tempa = (REAL*) malloc(sizeof(REAL)*10);
+		REAL * tempdzyx = (REAL*) malloc(sizeof(REAL)*258*258*258);
+		// MPI_Recv(&number, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		// MPI_Recv(tempa, 10, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Recv(tempdzyx, 258*258*258, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		PPF_Print( MPI_COMM_WORLD,"Process 1 received number %f from process 0\n", tempdzyx[0]);
+		// PPF_Print( MPI_COMM_WORLD,"Processor receive succes\n");
+	}
 
 	PPF_Print( MPI_COMM_WORLD, "Message from %N\n" );
 	PPF_Print( MPI_COMM_WORLD, (mpi_rank < (mpi_size/2) )
